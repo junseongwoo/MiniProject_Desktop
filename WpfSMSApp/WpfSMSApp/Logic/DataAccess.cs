d5cb96dd8ba72053444699eeef6d9e7084fc7331
junseongwoo <wnstjd4852@gmail.com>
2021-03-29 15:08:12 +0900

    ERP 학습 과정 커밋

---
+++ WpfSMSApp/WpfSMSApp/Logic/DataAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfSMSApp.Model;
+
+namespace WpfSMSApp.Logic
+{
+    public class DataAccess
+    {
+        public static List<User> GetUesrs()
+        {
+            List<User> users;
+
+            using (var ctx = new SMSEntities())
+            {
+                users = ctx.User.ToList(); // SELECT * FROM user 와 동일하다
+            }
+
+            return users;
+        }
+    }
+}

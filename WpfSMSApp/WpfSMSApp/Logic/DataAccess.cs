673de48c94b532c74bcdb1a88f122466579a8bc4
junseongwoo <wnstjd4852@gmail.com>
2021-03-30 17:24:53 +0900

    SMS 0330 커밋

--- WpfSMSApp/WpfSMSApp/Logic/DataAccess.cs
+++ WpfSMSApp/WpfSMSApp/Logic/DataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,15 @@ namespace WpfSMSApp.Logic
             }
 
             return users;
+        }
+
+        internal static int SetUser(User user)
+        {
+            using (var ctx = new SMSEntities())
+            {
+                ctx.User.AddOrUpdate(user);
+                return ctx.SaveChanges();    // commit
+            }
         }
     }
 }

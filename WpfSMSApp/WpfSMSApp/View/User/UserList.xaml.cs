b96546cea2c8fc40a897b27abb9107812784580a
junseongwoo <wnstjd4852@gmail.com>
2021-03-31 13:16:16 +0900

    0331 SMS 오전 커밋

--- WpfSMSApp/WpfSMSApp/View/User/UserList.xaml.cs
+++ WpfSMSApp/WpfSMSApp/View/User/UserList.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
@@ -19,19 +21,12 @@ namespace WpfSMSApp.View.User
         {
             try
             {
-                var user = Commons.LOGINED_USER;
-                /*TxtUserID.Text = user.UserID.ToString();
-                TxtUserIdentityNumber.Text = user.UserIdentityNumber.ToString();
-                TxtUserSurName.Text = user.UserSurname.ToString(); 
-                TxtUserName.Text = user.UserName.ToString(); ;
-                TxtUserEmail.Text = user.UserEmail.ToString(); ;
-                TxtUserAdmin.Text = user.UserAdmin.ToString();
-                TxtUserActivated.Text = user.UserActivated.ToString();*/
+                RdoAll.IsChecked = true;
 
             }
             catch (Exception ex)
             {
-                Commons.LOGGER.Error($"예외발생 MyAccount Loded : {ex}");
+                Commons.LOGGER.Error($"예외발생 UserList Loded : {ex}");
                 throw ex;
             }
         }
@@ -43,7 +38,16 @@ namespace WpfSMSApp.View.User
 
         private void BtnAddUser_Click(object sender, RoutedEventArgs e)
         {
-
+            
+            try
+            {
+                NavigationService.Navigate(new AddUser());
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnAddUsser_Click :{ex}");
+                throw;
+            }
         }
 
         private void BtnEditUser_Click(object sender, RoutedEventArgs e)
@@ -59,6 +63,65 @@ namespace WpfSMSApp.View.User
         private void BtnExportPdf_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void RdoAll_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<Model.User> users = new List<Model.User>();
+
+                if (RdoAll.IsChecked == true)
+                {
+                    users = Logic.DataAccess.GetUesrs();
+                }
+
+                this.DataContext = users;
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 : {ex}");
+            }
+        }
+
+        private void RdoActive_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<Model.User> users = new List<Model.User>();
+
+                if (RdoActive.IsChecked == true)
+                {
+                    // user에서 활성화 된 사람만 보게하는것
+                    users = Logic.DataAccess.GetUesrs().Where(u => u.UserActivated == true).ToList();
+                    
+                }
+
+                this.DataContext = users;
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 : {ex}");
+            }
+        }
+
+        private void RdoDeactive_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<Model.User> users = new List<Model.User>();
+
+                if (RdoDeactive.IsChecked == true)
+                {
+                    users = Logic.DataAccess.GetUesrs().Where(u => u.UserActivated == false).ToList();
+                }
+
+                this.DataContext = users;
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 : {ex}");
+            }
         }
     }
 }
